e60adb0840fe6a54c205554f58163b2b4e1a904a
Tanner Lemon <48069584+tanrlemn@users.noreply.github.com>
2024-09-10 11:49:34 -0400

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,42 @@ import Footer from './_navigation/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const APP_NAME = 'Redline Labs';
+const APP_DEFAULT_TITLE = 'Redline Labs – Testing Unmet Needs';
+const APP_TITLE_TEMPLATE = '%s – Redline Labs';
+const APP_DESCRIPTION =
+  'Redline pressure tests unmet needs for potential spinout opportunities, the rest get redlined.';
+
 export const metadata: Metadata = {
-  title: 'Redline - Testing Unmet Needs',
-  description:
-    'Redline pressure tests unmet needs for potential spinout opportunities, the rest get redlined.',
+  metadataBase: new URL('https://redlinelabs.co/'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  formatDetection: {
+    telephone: false,
+  },
+  category: 'Business',
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    images: [
+      'https://cdn.midjourney.com/aad85ee6-c0de-4d91-baab-0effc5257b81/0_0.png',
+    ],
+  },
 };
 
 export default function RootLayout({
